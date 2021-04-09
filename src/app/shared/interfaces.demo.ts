a650929f5dc2c30b8ebf12ecb1ecd35b7d30bb94
sreenivasguttula <53604598+sreenivasguttula@users.noreply.github.com>
2021-04-09 16:02:47 +0530

    second commit

--- src/app/shared/interfaces.demo.ts
+++ src/app/shared/interfaces.demo.ts
@@ -24,16 +24,20 @@ export class Demo{
       return params+" Hello"
      }
 
-     function f2(one:string,two:string) {
 
-      return one+two;
+
+     function f3(one:string,...multi:number[]) {
+
      }
 
      //let fun:FuncationlInterface = name;
-     let f:FuncationlInterface = f2;
+     //let f:FuncationlInterface = f2;
 
      //fun("HI")
      f("Hello","Hi")
+
+     let tupple:[string,number]=["sgsdg",100]
+     tupple.push(1);
 
    }
 }
